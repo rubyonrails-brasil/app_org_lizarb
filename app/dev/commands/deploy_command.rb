09394e643da9f0aa080b90821c542c5a70b94291
Thiago Pinto <thiagopintodev@gmail.com>
2022-10-21 13:24:34 -0300

    deploy de site estatico completo com setup e templates ERB

--- app/dev/commands/deploy_command.rb
+++ app/dev/commands/deploy_command.rb
@@ -1,14 +1,29 @@
 class DeployCommand < AppCommand
 
   set :user,       ENV["dev.deploy.user"]
+  set :site,       ENV["dev.deploy.site"]
   set :host,       ENV["dev.deploy.host"]
-  set :path,       ENV["dev.deploy.path"]
+  set :port,       ENV["dev.deploy.port"]
 
   def self.call args
+    @user = get :user
+    @site = get :site
+    @host = get :host
+    @port = get :port
+
     clear_cache
-    cache RootRequest
-    deploy
+
+    if args[0] == "setup"
+      deploy_nginx_site
+      install_nginx_site
+      call []
+    else
+      cache_request RootRequest
+      deploy_cached_requests
+    end
   end
+
+  #
 
   def self.clear_cache
     log "Clearing cache"
@@ -16,7 +31,35 @@ class DeployCommand < AppCommand
     FileUtils.mkdir_p tmp_folder, verbose: true
   end
 
-  def self.cache request_klass
+  #
+
+  def self.deploy_nginx_site
+    path_nginx_site = "#{tmp_folder}/#{@site}.nginx"
+    erb_template = File.read "#{App.fname_for self}/site.nginx.erb"
+    content_nginx_site = ERB.new(erb_template).result(binding)
+    File.write path_nginx_site, content_nginx_site
+
+    puts "\n\n#{content_nginx_site}".black.on_light_green
+
+    log "deploying nginx site"
+    Kernel.system %|scp #{path_nginx_site} #{@user}@#{@host}:.|
+  end
+
+  def self.install_nginx_site
+    path_setup_sh = "#{tmp_folder}/#{@site}.setup.sh"
+    erb_template = File.read "#{App.fname_for self}/setup.sh.erb"
+    content_nginx_site = ERB.new(erb_template).result(binding)
+    File.write path_setup_sh, content_nginx_site
+
+    puts "\n\n#{content_nginx_site}".black.on_light_green
+
+    log "installing nginx site"
+    Kernel.system "cat #{path_setup_sh} | ssh #{@user}@#{@host}"
+  end
+
+  #
+
+  def self.cache_request request_klass
     request_klass::ACTIONS.keys.each do |action|
       log "Writing cache for #{request_klass} action: #{action}"
 
@@ -37,22 +80,20 @@ class DeployCommand < AppCommand
     end
   end
 
-  def self.deploy
-    user = get :user
-    host = get :host
-    path = get :path
-
+  def self.deploy_cached_requests
     log "deploying cached requests"
-    Kernel.system %|scp #{tmp_folder}/* #{user}@#{host}:#{path}.|
+    Kernel.system %|scp #{tmp_folder}/* #{@user}@#{@host}:#{@site}/.|
 
     log "deploying web_files"
-    Kernel.system %|scp web_files/* #{user}@#{host}:#{path}.|
+    Kernel.system %|scp web_files/* #{@user}@#{@host}:#{@site}/.|
 
     log "done"
   end
 
+  #
+
   def self.tmp_folder
-    ENV["dev.deploy.tmp_folder"]
+    "tmp/deploy_files"
   end
 
 end
